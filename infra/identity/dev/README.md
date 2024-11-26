0cea34ff5c73295ba340398a4dc1f36e172b01bd
Marco Comi <marco.comi@pagopa.it>
2024-11-26 16:16:55 +0100

    Create identity

---
+++ infra/identity/dev/README.md
@@ -0,0 +1,1 @@
+# DX Playground - GitHub federated Managed Identities
---
+++ infra/identity/dev/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "<= 3.100.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "tfdevdx"
+    container_name       = "terraform-state"
+    key                  = "dx-playground.identity.dev.italynorth.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "federated_identities" {
+  source = "github.com/pagopa/dx//infra/modules/azure_federated_identity_with_github?ref=19b6c8a118cdd60671d603dac87d3663089d72a7"
+
+  prefix    = local.prefix
+  env_short = local.env_short
+  env       = local.env
+  domain    = local.domain
+
+  repositories = [local.repo_name]
+
+  tags = local.tags
+}
