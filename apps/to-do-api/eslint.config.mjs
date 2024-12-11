89ec2e791426963e243ca5f257cf1194df3d7a27
Marco Comi <marco.comi@pagopa.it>
2024-12-11 16:54:11 +0100

    Ignore some eslint rules

--- apps/to-do-api/eslint.config.mjs
+++ apps/to-do-api/eslint.config.mjs
@@ -1,3 +1,15 @@
 import lintRules from "@pagopa/eslint-config";
 
-export default lintRules;
+export default [
+  ...lintRules,
+  {
+    rules: {
+      // The following rules are raising errors when running the lint command
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+    },
+  },
+  {
+    ignores: ["dist/*"],
+  },
+];
