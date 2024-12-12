7852c746ce5b31418d13d4df2fe979ff5667b605
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-12-12 14:27:09 +0100

    Use eslint v8 (#31)

--- apps/to-do-api/eslint.config.js
+++ apps/to-do-api/eslint.config.js
@@ -3,13 +3,6 @@ import lintRules from "@pagopa/eslint-config";
 export default [
   ...lintRules,
   {
-    rules: {
-      // The following rules are raising errors when running the lint command
-      "@typescript-eslint/no-empty-function": "off",
-      "@typescript-eslint/no-unused-expressions": "off",
-    },
-  },
-  {
     ignores: ["dist/*"],
   },
 ];
