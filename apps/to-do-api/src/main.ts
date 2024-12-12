972042f1ef4783f4165d2ef1a283baa1070fa003
Marco Comi <marco.comi@pagopa.it>
2024-12-12 15:29:25 +0100

    Ping Cosmos DB when invoke GET /info

--- apps/to-do-api/src/main.ts
+++ apps/to-do-api/src/main.ts
@@ -1,10 +1,28 @@
+import { CosmosClient } from "@azure/cosmos";
 import { app } from "@azure/functions";
+import { DefaultAzureCredential } from "@azure/identity";
+import * as E from "fp-ts/lib/Either.js";
+import { pipe } from "fp-ts/lib/function.js";
 
 import { makeInfoHandler } from "./adapters/azure/functions/info.js";
+import { getConfigOrError } from "./config.js";
+
+const config = pipe(
+  getConfigOrError(process.env),
+  E.getOrElseW((error) => {
+    throw error;
+  }),
+);
+
+const aadCredentials = new DefaultAzureCredential();
+const cosmosClient = new CosmosClient({
+  aadCredentials,
+  endpoint: config.cosmosDb.endpoint,
+});
 
 app.http("info", {
   authLevel: "anonymous",
-  handler: makeInfoHandler({}),
+  handler: makeInfoHandler({ cosmosClient }),
   methods: ["GET"],
   route: "info",
 });
