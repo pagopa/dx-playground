a13ceb3d800fa9ea5ad029b591601e19bb8c8ae3
Mario Mupo <mario.mupo@pagopa.it>
2024-12-17 12:06:21 +0100

    fix: added roles

--- infra/identity/dev/main.tf
+++ infra/identity/dev/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "<= 3.100.0"
+      version = "<= 3.117.0"
     }
   }
 
@@ -29,4 +29,34 @@ module "federated_identities" {
   repositories = [local.repo_name]
 
   tags = local.tags
+}
+
+module "roles_ci" {
+  source       = "github.com/pagopa/dx//infra/modules/azure_role_assignments?ref=19b6c8a118cdd60671d603dac87d3663089d72a7"
+  principal_id = module.federated_identities.federated_ci_identity.id
+
+  key_vault = [
+    {
+      name                = "${local.project}-${local.location_short}-common-kv-01"
+      resource_group_name = "${local.project}-${local.location_short}-common-rg-01"
+      roles = {
+        secrets = "reader"
+      }
+    }
+  ]
+}
+
+module "roles_cd" {
+  source       = "github.com/pagopa/dx//infra/modules/azure_role_assignments?ref=19b6c8a118cdd60671d603dac87d3663089d72a7"
+  principal_id = module.federated_identities.federated_cd_identity.id
+
+  key_vault = [
+    {
+      name                = "${local.project}-${local.location_short}-common-kv-01"
+      resource_group_name = "${local.project}-${local.location_short}-common-rg-01"
+      roles = {
+        secrets = "reader"
+      }
+    }
+  ]
 }
