b9732381879f5c2075e1463a34a87ffc6255dc88
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-12-20 11:09:34 +0100

    Update infra/resources/_modules/api/policy.tf

--- infra/resources/_modules/api/policy.tf
+++ infra/resources/_modules/api/policy.tf
@@ -1,4 +1,4 @@
-resource "azurerm_api_management_api_policy" "to_do_api" {
+resource "azurerm_api_management_api_policy" "policy" {
   api_name            = azurerm_api_management_api.api.name
   api_management_name = azurerm_api_management_api.api.api_management_name
   resource_group_name = azurerm_api_management_api.api.resource_group_name
