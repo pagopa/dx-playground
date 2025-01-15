eb3136621b10cd6998dd10797dccfcd01faa84c9
Marco Comi <marco.comi@pagopa.it>
2025-01-15 17:14:04 +0100

    Move function into a separate file

--- apps/to-do-api/src/adapters/azure/cosmosdb/decode.ts
+++ apps/to-do-api/src/adapters/azure/cosmosdb/decode.ts
@@ -1,4 +1,5 @@
-import { FeedResponse } from "@azure/cosmos";
+import { FeedResponse, ItemDefinition, ItemResponse } from "@azure/cosmos";
+import * as O from "fp-ts/Option";
 import * as E from "fp-ts/lib/Either.js";
 import { pipe } from "fp-ts/lib/function.js";
 import * as t from "io-ts";
@@ -17,5 +18,25 @@ export const decodeFromFeed =
       E.mapLeft(
         () =>
           new Error(`Unable to parse the resources using codec ${codec.name}`),
+      ),
+    );
+
+/**
+ * Decode a single resource, extracted from an ItemResponse, using a codec.
+ * @param codec the io-ts codec to use to decode the resource
+ */
+export const decodeFromItem =
+  <A, O>(codec: t.Type<A, O>) =>
+  <T extends ItemDefinition>(item: ItemResponse<T>) =>
+    pipe(
+      O.fromNullable(item.resource),
+      O.map(codec.decode),
+      // transform Option<Either<L, R>> => Either<L, Option<R>>
+      O.sequence(E.Applicative),
+      E.mapLeft(
+        () =>
+          new Error(
+            `Unable to parse the ${item.resource?.id} using codec ${codec.name}`,
+          ),
       ),
     );
