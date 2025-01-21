05db4c6e43dad8dba511cc2d6b7093dc114a96aa
Marco Comi <marco.comi@pagopa.it>
2025-01-21 12:47:19 +0100

    Use DX Action to deploy web app

--- apps/to-do-webapp/next.config.ts
+++ apps/to-do-webapp/next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
-  distDir: 'build',
+  output: "standalone"
 };
 
 export default nextConfig;
