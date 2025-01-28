90785fd04c3bdba2c5b9f38c163e8f199a0a0cd4
Marco Comi <9998393+kin0992@users.noreply.github.com>
2025-01-28 10:25:11 +0100

    [CES-548] Add dynatrace script (#99)

---
+++ .changeset/tasty-taxis-juggle.md
@@ -0,0 +1,5 @@
+---
+"to-do-webapp": patch
+---
+
+[CES-548] Add Dynatrace js
--- apps/to-do-webapp/src/app/layout.tsx
+++ apps/to-do-webapp/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 
+import Script from "next/script";
+
 export const metadata: Metadata = {
   description: "A To Do List app",
   title: "To Do List",
@@ -13,6 +15,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <Script
+        crossOrigin="anonymous"
+        id="dynatrace"
+        src="https://js-cdn.dynatrace.com/jstag/16827775e4e/bf66232jyn/6c925e7eb2402285_complete.js"
+        type="text/javascript"
+      ></Script>
     </html>
   );
 }
