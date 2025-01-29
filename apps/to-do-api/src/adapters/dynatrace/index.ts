03b9bc8454635bf69dd93851174dc068d0523891
Marco Comi <marco.comi@pagopa.it>
2025-01-29 10:35:42 +0100

    Instrument azure sdk for opentelemetry

--- apps/to-do-api/src/adapters/dynatrace/index.ts
+++ apps/to-do-api/src/adapters/dynatrace/index.ts
@@ -1,6 +1,13 @@
+import { createAzureSdkInstrumentation } from "@azure/opentelemetry-instrumentation-azure-sdk";
 import { initDynatrace } from "@dynatrace/opentelemetry-azure-functions";
+import { registerInstrumentations } from "@opentelemetry/instrumentation";
 
 export const setupDynatrace = () => {
   // initDynatrace with OpenTelemetry setup (recommended)
   initDynatrace(true);
+
+  // Register Azure SDK Instrumentation
+  registerInstrumentations({
+    instrumentations: [createAzureSdkInstrumentation()],
+  });
 };
