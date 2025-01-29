430dc0f71f91828d78aae9324eab6a18097b265c
Marco Comi <marco.comi@pagopa.it>
2025-01-29 16:06:24 +0100

    Add NODE_OPTIONS environment variable to Function App

--- infra/resources/dev/function_app.tf
+++ infra/resources/dev/function_app.tf
@@ -7,10 +7,12 @@ locals {
     COSMOSDB_TASKS_CONTAINER_NAME = azurerm_cosmosdb_sql_container.tasks.name
 
     # Dynatrace
-    DT_TENANT = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.dynatrace_tenant.versionless_id})"
-    DT_CLUSTER_ID : "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.dynatrace_cluster_id.versionless_id})"
-    DT_CONNECTION_BASE_URL : "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.dynatrace_target_url.versionless_id})"
-    DT_CONNECTION_AUTH_TOKEN : "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.dynatrace_connection_auth_token.versionless_id})"
+    DT_TENANT                = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.dynatrace_tenant.versionless_id})"
+    DT_CLUSTER_ID            = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.dynatrace_cluster_id.versionless_id})"
+    DT_CONNECTION_BASE_URL   = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.dynatrace_target_url.versionless_id})"
+    DT_CONNECTION_AUTH_TOKEN = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.dynatrace_connection_auth_token.versionless_id})"
+
+    "NODE_OPTIONS" = "--experimental-loader=@opentelemetry/instrumentation/hook.mjs",
   }
 }
 
