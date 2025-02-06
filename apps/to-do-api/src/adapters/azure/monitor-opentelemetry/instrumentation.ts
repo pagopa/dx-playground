0ed67a1f9b00414b60daaf7401748c9ae7362b16
Marco Comi <marco.comi@pagopa.it>
2025-02-06 15:33:01 +0100

    Rollback to AI

--- apps/to-do-api/src/adapters/azure/monitor-opentelemetry/instrumentation.ts
+++ apps/to-do-api/src/adapters/azure/monitor-opentelemetry/instrumentation.ts
@@ -1,34 +1,13 @@
-import {
-  AzureMonitorOpenTelemetryOptions,
-  useAzureMonitor,
-} from "@azure/monitor-opentelemetry";
-import { createAzureSdkInstrumentation } from "@azure/opentelemetry-instrumentation-azure-sdk";
 import { metrics, trace } from "@opentelemetry/api";
-import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import { UndiciInstrumentation } from "@opentelemetry/instrumentation-undici";
+import appInsights from "applicationinsights";
 import { instrumentAzureFunctions } from "azure-functions-otel-instrumentation/dist/index.js";
-const options: AzureMonitorOpenTelemetryOptions = {
-  instrumentationOptions: {
-    azureSdk: {
-      enabled: true,
-    },
-    http: {
-      enabled: true,
-    },
-  },
-  samplingRatio: Number(process.env.APPINSIGHTS_SAMPLING_PERCENTAGE) / 100 || 1,
-};
 
-useAzureMonitor(options);
+appInsights.setup().start();
 
 registerInstrumentations({
-  instrumentations: [
-    getNodeAutoInstrumentations(),
-    instrumentAzureFunctions(),
-    createAzureSdkInstrumentation(),
-    new UndiciInstrumentation(),
-  ],
+  instrumentations: [instrumentAzureFunctions(), new UndiciInstrumentation()],
   meterProvider: metrics.getMeterProvider(),
   tracerProvider: trace.getTracerProvider(),
 });
