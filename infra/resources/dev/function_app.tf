c9d4533be5fc4abec3960c8d3d75cb80ab0e90e2
Marco Comi <marco.comi@pagopa.it>
2025-02-11 11:31:26 +0100

    Remove NODE_OPTIONS variable

--- infra/resources/dev/function_app.tf
+++ infra/resources/dev/function_app.tf
@@ -5,8 +5,6 @@ locals {
 
     # Cosmos Container Names
     COSMOSDB_TASKS_CONTAINER_NAME = azurerm_cosmosdb_sql_container.tasks.name
-
-    NODE_OPTIONS = "--import ./dist/adapters/azure/instrumentation.mjs"
   }
 }
 
