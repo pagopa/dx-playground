c2c8db1378af3fb5fd2e801d739cc94692486fbb
Marco Comi <marco.comi@pagopa.it>
2025-02-14 10:05:19 +0100

    Enable AI on App Service

--- infra/resources/dev/app_service.tf
+++ infra/resources/dev/app_service.tf
@@ -27,6 +27,9 @@ module "app_service" {
 
   health_check_path = "/"
 
+  application_insights_connection_string   = "@Microsoft.KeyVault(SecretUri=${module.application_insights.connection_string_secret_id})"
+  application_insights_sampling_percentage = 100
+
   tags = local.tags
 }
 
