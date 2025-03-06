03b1ef5f261d9dcf903af5a809755723c22d4d57
Marco Comi <marco.comi@pagopa.it>
2025-03-06 10:38:04 +0100

    Remove instrumentation_key as module's output

--- infra/resources/_modules/application_insights/outputs.tf
+++ infra/resources/_modules/application_insights/outputs.tf
@@ -2,9 +2,3 @@ output "connection_string_secret_id" {
   value       = azurerm_key_vault_secret.ai_connection_string.versionless_id
   description = "The id of the secret containing the connection string"
 }
-
-output "instrumentation_key" {
-  value       = azurerm_application_insights.main.instrumentation_key
-  sensitive   = true
-  description = "The instrumentation key of Application Insights"
-}
