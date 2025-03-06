903664cdeb5515f2cf769f7ee46fa929bfb73c5e
Marco Comi <marco.comi@pagopa.it>
2025-03-06 16:36:52 +0100

    Add log_analytics_workspace_id as application insights module output

--- infra/resources/_modules/application_insights/outputs.tf
+++ infra/resources/_modules/application_insights/outputs.tf
@@ -7,3 +7,8 @@ output "connection_string" {
   value       = azurerm_application_insights.main.connection_string
   description = "The connection string to the Application Insights instance"
 }
+
+output "log_analytics_workspace_id" {
+  value       = azurerm_log_analytics_workspace.main.id
+  description = "The id of the Log Analytics Workspace"
+}
