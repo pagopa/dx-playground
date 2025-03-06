d30b948ce4210689dc35a231615c7281f621f8c4
Marco Comi <marco.comi@pagopa.it>
2025-03-06 10:24:35 +0100

    Get naming convention module from registry

--- infra/resources/dev/main.tf
+++ infra/resources/dev/main.tf
@@ -21,6 +21,8 @@ provider "azurerm" {
 }
 
 module "naming_convention" {
-  source      = "github.com/pagopa/dx//infra/modules/azure_naming_convention?ref=main"
+  source  = "pagopa/dx-azure-naming-convention/azurerm"
+  version = "0.0.5"
+
   environment = merge(local.environment, { app_name = "pg" })
 }
