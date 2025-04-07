010ea52cecef7904867e64b887f6d66afbd2bf3d
Marco Comi <marco.comi@pagopa.it>
2025-04-07 17:36:10 +0200

    Use @pagopa/azure-tracing package in webapp workspace

--- apps/to-do-webapp/src/instrumentation.ts
+++ apps/to-do-webapp/src/instrumentation.ts
@@ -1,14 +1,5 @@
-import { useAzureMonitor } from "@azure/monitor-opentelemetry";
-import { metrics, trace } from "@opentelemetry/api";
-import { registerInstrumentations } from "@opentelemetry/instrumentation";
-import { UndiciInstrumentation } from "@opentelemetry/instrumentation-undici";
+import { init } from "@pagopa/azure-tracing/azure-monitor";
 
 export async function register() {
-  useAzureMonitor();
-
-  registerInstrumentations({
-    instrumentations: [new UndiciInstrumentation()],
-    meterProvider: metrics.getMeterProvider(),
-    tracerProvider: trace.getTracerProvider(),
-  });
+  init();
 }
