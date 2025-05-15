f73dff276e28e41d7b99444d5e0c8d3bc7814e5f
Marco Comi <marco.comi@pagopa.it>
2025-05-15 11:29:59 +0200

    Add first implementation of get task list endpoint

---
+++ apps/azure-function-v3/src/adapters/express/index.ts
@@ -0,0 +1,32 @@
+import express, {Express} from 'express';
+import {AzureFunction, Context} from "@azure/functions";
+import createAzureFunctionHandler from "@pagopa/express-azure-functions/dist/src/createAzureFunctionsHandler.js";
+
+type Task = {
+  id: string;
+  state: 'COMPLETED' | 'DELETED' | 'INCOMPLETE';
+  title: string;
+}
+
+export const makeExpressApp = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.get('/api/tasks', (req, res) => {
+    const taskList: readonly Task[] = [];
+    res.json(taskList)
+  });
+
+  return app;
+}
+
+export const setAppContext = (app: express.Express, context: Context): void => {
+  app.set("context", context);
+};
+
+export const expressToAzureFunction =
+  (app: Express): AzureFunction =>
+    (context: Context): void => {
+      setAppContext(app, context);
+      createAzureFunctionHandler.default(app)(context);
+    };
---
+++ apps/azure-function-v3/src/main.ts
@@ -0,0 +1,7 @@
+import {
+  expressToAzureFunction,
+  makeExpressApp
+} from "./adapters/express/index.js";
+
+const app = makeExpressApp();
+export const entryPoint = expressToAzureFunction(app)
