8b41f259b14d55be6efb415fb32e3fea0852fe14
Marco Comi <9998393+kin0992@users.noreply.github.com>
2025-05-16 15:44:29 +0200

    Implements GET /tasks under Azure Function v3 (#183)
    Closes CES-1010

---
+++ apps/azure-function-v3/src/adapters/express/index.ts
@@ -0,0 +1,28 @@
+import { AzureFunction, Context } from "@azure/functions";
+import createAzureFunctionHandler from "@pagopa/express-azure-functions/dist/src/createAzureFunctionsHandler.js";
+import { Capabilities, listTasks } from "@to-do/domain";
+import express, { Express } from "express";
+import * as E from "fp-ts/lib/Either.js";
+
+export const makeExpressApp = (env: Capabilities) => {
+  const app = express();
+
+  app.use(express.json());
+  app.get("/api/tasks", async (req, res) => {
+    const tasks = await listTasks()(env)();
+    if (E.isLeft(tasks)) {
+      return res.status(500).json({ message: "Error fetching tasks" });
+    } else {
+      res.json(tasks.right);
+    }
+  });
+
+  return app;
+};
+
+export const expressToAzureFunction =
+  (app: Express): AzureFunction =>
+  (context: Context): void => {
+    app.set("context", context);
+    createAzureFunctionHandler.default(app)(context);
+  };
