bce053318a2aa42cd84da65738cc1a3757f8e2f1
Marco Comi <marco.comi@pagopa.it>
2025-05-30 15:25:09 +0200

    Craete action in Typescript

---
+++ apps/release-candidate/src/index.ts
@@ -0,0 +1,53 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { enterPre, exitPre } from "@changesets/pre";
+import {
+  publish as publishPackages,
+  readChangesetState,
+  version as versionPackages,
+} from "@changesets/release-utils";
+
+const run = async () => {
+  const pr = github.context.payload.pull_request;
+  core.info(`Preparing a pre-release or PR ${pr?.number}`);
+
+  const workDir = core.getInput("workDir", {
+    required: true,
+    trimWhitespace: true,
+  });
+  const preReleaseTag = core.getInput("releaseType", {
+    required: true,
+    trimWhitespace: true,
+  });
+  core.debug(`Working directory: ${workDir}`);
+
+  const { changesets, preState } = await readChangesetState(workDir);
+  core.debug(`PreState: ${JSON.stringify(preState, null, 2)}`);
+  core.debug(`Changesets: ${JSON.stringify(changesets, null, 2)}`);
+
+  if (!preState || preState.mode === "exit") {
+    core.info("It is possible to enter the pre-release mode");
+    core.info(`Enter pre-release mode with ${preReleaseTag} tag`);
+    await enterPre(workDir, preReleaseTag);
+
+    core.info("Versioning the packages...");
+    const versionResult = await versionPackages({
+      commitMessage: `Prepare packages ${preReleaseTag} version`,
+      cwd: workDir,
+    });
+    core.debug(`Version result: ${JSON.stringify(versionResult, null, 2)}`);
+    core.info("Publishing the packages...");
+    const publishResult = await publishPackages({
+      cwd: workDir,
+      script: "publish",
+    });
+    core.debug(`Publish result: ${JSON.stringify(publishResult, null, 2)}`);
+
+    core.info("Exit pre-release mode");
+    await exitPre(workDir);
+  } else {
+    core.warning("Already in pre-release mode");
+  }
+};
+
+run();
