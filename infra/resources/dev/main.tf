7eca46233de8ccd4342916594dafb1d6c261efbe
Danilo Spinelli <danilo.spinelli@pagopa.it>
2025-06-04 22:55:55 +0200

    Add storage account module configuration to main.tf

--- infra/resources/dev/main.tf
+++ infra/resources/dev/main.tf
@@ -32,3 +32,32 @@ module "naming_convention" {
 }
 
 data "azurerm_subscription" "current" {}
+
+#
+# Storage Account
+#
+
+module "storage_account" {
+  source  = "pagopa-dx/azure-storage-account/azurerm"
+  version = "~> 1"
+
+  environment = {
+    prefix          = local.environment.prefix
+    env_short       = local.environment.env_short
+    location        = local.environment.location
+    domain          = local.environment.domain
+    app_name        = "unbuffered"
+    instance_number = local.environment.instance_number
+  }
+
+  resource_group_name = data.azurerm_resource_group.test_rg.name
+
+  # Use dev tier for development environment
+  tier = "s"
+
+  # Configure private endpoint integration
+  subnet_pep_id                        = data.azurerm_subnet.pep_snet.id
+  private_dns_zone_resource_group_name = data.azurerm_virtual_network.test_vnet.resource_group_name
+
+  tags = local.tags
+}
