80091ded3d08ab9eb390d825c21eae0dc4d4d8b8
Danilo Spinelli <danilo.spinelli@pagopa.it>
2025-06-05 11:40:18 +0200

    Add working-directory input to Terraform Plan action and use it in spawn command

--- .github/actions/terraform-plan-stream/index.js
+++ .github/actions/terraform-plan-stream/index.js
@@ -5,68 +5,85 @@ const { Transform } = require('stream');
 
 async function run() {
   try {
+    // 1. LEGGI L'INPUT DAL WORKFLOW
+    // Il nome 'working-directory' deve corrispondere a quello definito in action.yml
+    const workingDir = core.getInput('working-directory', { required: true });
+
     const logFile = 'plan_output.txt';
     const filterString = 'hidden-link:';
 
-    core.info(`Azione avviata nella dir ${process.cwd()}. L'output verrà scritto anche su ${logFile}`);
+    core.info(`Azione avviata.`);
+    core.info(`Working Directory: ${workingDir}`); // Log per debug
+    core.info(`L'output verrà scritto anche su ${logFile}`);
 
     const logStream = createWriteStream(logFile);
 
-    // Manteniamo stdbuf come best practice per essere sicuri al 100%
-    // che il processo figlio stesso non stia bufferizzando.
     const command = 'stdbuf';
     const args = ['-oL', 'terraform', 'plan', '-lock-timeout=3000s', '-no-color'];
-    const terraformProcess = spawn(command, args);
 
-    const lineFilter = new Transform({
-      transform(chunk, encoding, callback) {
-        const lines = (this.carry || '' + chunk.toString()).split(/\r?\n/);
-        this.carry = lines.pop();
-        for (const line of lines) {
-          if (!line.includes(filterString)) {
-            this.push(line + '\n');
-          }
-        }
-        callback();
-      },
-      flush(callback) {
-        if (this.carry && !this.carry.includes(filterString)) {
-          this.push(this.carry);
-        }
-        callback();
-      }
-    });
+    // 2. USA L'INPUT COME CWD PER SPAWN
+    const terraformProcess = spawn(command, args, { cwd: workingDir });
 
-    // Usiamo core.info per il logging, che è il modo idiomatico per un'azione.
-    // Questo potrebbe avere un comportamento di flushing diverso e più affidabile.
-    terraformProcess.stdout.pipe(lineFilter).on('data', (data) => {
-        process.stdout.write(data); // Scrive direttamente allo stdout del processo Node
-        logStream.write(data);
-    });
+    // ... il resto dello script rimane identico ...
 
-    terraformProcess.stderr.on('data', (data) => {
-        process.stderr.write(data); // Scrive direttamente allo stderr del processo Node
-        logStream.write(data);
-    });
-    
-    // Gestione della conclusione del processo
-    terraformProcess.on('close', (code) => {
-      logStream.end();
-      if (code !== 0) {
-        core.setFailed(`Il processo Terraform è terminato con codice ${code}`);
-      } else {
-        core.info('Processo Terraform completato con successo.');
-      }
-    });
-
-    terraformProcess.on('error', (err) => {
-        logStream.end();
-        core.setFailed(`Errore nell'avvio del processo: ${err.message}`);
-    });
+    const lineFilter = new Transform({ /* ... */ });
+    terraformProcess.stdout.pipe(lineFilter).on('data', (data) => { /* ... */ });
+    terraformProcess.stderr.on('data', (data) => { /* ... */ });
+    terraformProcess.on('close', (code) => { /* ... */ });
+    terraformProcess.on('error', (err) => { /* ... */ });
 
   } catch (error) {
     core.setFailed(error.message);
   }
 }
+
+// Incolla qui il resto del codice da `lineFilter` in poi,
+// è identico alla versione precedente. Lo ometto per brevità.
+
+// --- Blocco di codice da incollare ---
+const lineFilter = new Transform({
+  transform(chunk, encoding, callback) {
+    const lines = (this.carry || '' + chunk.toString()).split(/\r?\n/);
+    this.carry = lines.pop();
+    for (const line of lines) {
+      if (!line.includes(filterString)) {
+        this.push(line + '\n');
+      }
+    }
+    callback();
+  },
+  flush(callback) {
+    if (this.carry && !this.carry.includes(filterString)) {
+      this.push(this.carry);
+    }
+    callback();
+  }
+});
+
+terraformProcess.stdout.pipe(lineFilter).on('data', (data) => {
+    process.stdout.write(data);
+    logStream.write(data);
+});
+
+terraformProcess.stderr.on('data', (data) => {
+    process.stderr.write(data);
+    logStream.write(data);
+});
+
+terraformProcess.on('close', (code) => {
+  logStream.end();
+  if (code !== 0) {
+    core.setFailed(`Il processo Terraform è terminato con codice ${code}`);
+  } else {
+    core.info('Processo Terraform completato con successo.');
+  }
+});
+
+terraformProcess.on('error', (err) => {
+    logStream.end();
+    core.setFailed(`Errore nell'avvio del processo: ${err.message}`);
+});
+// --- Fine blocco di codice ---
+
 
 run();
