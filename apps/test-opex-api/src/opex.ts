bc479cb654e198611286ec5ee541109ea64a408d
Danilo Spinelli <danilo.spinelli@pagopa.it>
2025-06-18 09:48:53 +0200

    refactor: Enhance opex.ts by restructuring MonitoringStack instances and adding AzurermBackend configurations

--- apps/test-opex-api/src/opex.ts
+++ apps/test-opex-api/src/opex.ts
@@ -1,12 +1,34 @@
-import { App } from "cdktf";
+import { App, AzurermBackend } from "cdktf";
 import { MonitoringStack } from "cdktf-monitoring-stack";
-import { opexConfig } from "opex-common";
+import { backendConfig, opexConfig } from "opex-common";
 
-const app = new App();
-
-new MonitoringStack(app, "to-do-api-v2", {
-  config: opexConfig,
-  openApiFilePaths: ["docs/openapi-v2.yaml", "docs/openapi-v3.yaml"],
+const app = new App({
+  hclOutput: true,
+  outdir: `${process.cwd()}/../../infra/resources/opex`,
 });
+
+const stack1 = new MonitoringStack(app, "test-opex-api-v1", {
+  config: opexConfig,
+  openApiFilePaths: ["docs/openapi-v2.yaml"],
+});
+
+new AzurermBackend(stack1, {
+  ...backendConfig,
+  key: "dx.test-opex-api1.tfstate",
+});
+
+///////
+
+const stack2 = new MonitoringStack(app, "test-opex-api-v2", {
+  config: opexConfig,
+  openApiFilePaths: ["docs/openapi-v3.yaml"],
+});
+
+new AzurermBackend(stack2, {
+  ...backendConfig,
+  key: "dx.test-opex-api2.tfstate",
+});
+
+///////
 
 app.synth();
