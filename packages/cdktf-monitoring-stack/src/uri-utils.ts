83240d024da2bdb2196f566ac9eeb79aa5285fb8
Danilo Spinelli <danilo.spinelli@pagopa.it>
2025-06-18 09:54:16 +0200

    fix: Correct regex pattern in uriToRegex function to properly handle URI path parameters

--- packages/cdktf-monitoring-stack/src/uri-utils.ts
+++ packages/cdktf-monitoring-stack/src/uri-utils.ts
@@ -6,5 +6,5 @@
  * Translate path parameters of a URI to a generic version thanks to regex
  */
 export function uriToRegex(value: string): string {
-  return String(value).replace(/{[^/]+}/g, "[^/]+") + "$";
+  return String(value).replace(/{[^}]+}/g, "[^/]+") + "$";
 }
