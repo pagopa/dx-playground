f0e41509a6fe0320aa02b1caeddee7014cbc01d1
Danilo Spinelli <danilo.spinelli@pagopa.it>
2025-06-18 09:48:21 +0200

    refactor: Remove unused exports from index.ts for cleaner code structure

--- packages/cdktf-monitoring-stack/src/index.ts
+++ packages/cdktf-monitoring-stack/src/index.ts
@@ -1,38 +1,6 @@
-// Export dashboard functions and types
-export {
-  createChartPart,
-  generateDashboardProperties,
-} from "./dashboard-generator.js";
-export type { ChartOptions } from "./dashboard-generator.js";
-
-// Export main classes
 export {
   MonitoringConfig,
   MonitoringStack,
   MonitoringStackProps,
   Tags,
 } from "./monitoring-stack.js";
-
-// Export OpenAPI processing functions and types
-export {
-  endpointsWithDefaultProperties,
-  extractServerBasePaths,
-  extractServerUrls,
-  processOpenApiFiles,
-} from "./openapi-processor.js";
-export type {
-  EndpointDetails,
-  EndpointWithProperties,
-  OpenApiSpec,
-} from "./openapi-processor.js";
-
-// Export query builders and types
-export {
-  getApimAvailabilityQuery,
-  getApimResponseCodesQuery,
-  getApimResponseTimeQuery,
-} from "./query-builder.js";
-export type { QueryParams } from "./query-builder.js";
-
-// Export utility functions
-export { uriToRegex } from "./uri-utils.js";
