eb6723dacd7854f87b209bee1d1886e5e23f4e88
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-07-30 12:30:42 +0200

    Remove GetTasks

--- apps/azure-function-v3/src/adapters/express/index.ts
+++ apps/azure-function-v3/src/adapters/express/index.ts
@@ -1,23 +1,13 @@
 import { AzureFunction, Context as FunctionContext } from "@azure/functions";
 import { withOtelContextFunctionV3 } from "@pagopa/azure-tracing/azure-functions/v3";
 import createAzureFunctionHandler from "@pagopa/express-azure-functions/dist/src/createAzureFunctionsHandler.js";
-import { Capabilities, listTasks } from "@to-do/domain";
-import express, { Express } from "express";
-import * as E from "fp-ts/lib/Either.js";
 
-export const makeExpressApp = (env: Capabilities) => {
+import express, { Express } from "express";
+
+export const makeExpressApp = () => {
   const app = express();
 
   app.use(express.json());
-
-  app.get("/api/tasks", async (req, res) => {
-    const tasks = await listTasks()(env)();
-    if (E.isLeft(tasks)) {
-      return res.status(500).json({ message: "Error fetching tasks" });
-    } else {
-      res.json(tasks.right);
-    }
-  });
 
   app.get("/api/health", (req, res) => {
     res.status(200).json({ status: "ok" });
