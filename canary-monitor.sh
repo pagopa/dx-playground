ba8d95f721b334746d038914fc9497c58b39a3c4
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-08-06 14:07:11 +0200

    Remove canary-monitor

--- canary-monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-currentPercentage=${3:-0}
-
-nextPercentage=$((currentPercentage + 20))
-afterMs=10000
-
-jq -n --arg nextPercentage "$nextPercentage" --arg afterMs "$afterMs" \
-  '{nextPercentage: $nextPercentage | tonumber, afterMs: $afterMs | tonumber}'
