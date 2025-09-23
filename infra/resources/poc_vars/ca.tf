0265f97d904264b860382c5c33be1ffdbc969006
Krusty93 <andrea.grillo@pagopa.it>
2025-09-23 11:17:26 +0200

    enable ca

--- infra/resources/poc_vars/ca.tf
+++ infra/resources/poc_vars/ca.tf
@@ -1,44 +1,65 @@
-# resource "azurerm_container_app" "this" {
-#   name                = "${local.project}-ca-01"
-#   location            = local.location
-#   resource_group_name = azurerm_resource_group.this.name
-#   container_app_environment_id = azurerm_container_app_environment.this.id
-#   workload_profile_name        = "Consumption"
-#   tags                = local.tags
+locals {
+  port = 8080
+}
 
-#   identity {
-#     type = "SystemAssigned"
-#   }
+resource "azurerm_container_app" "this" {
+  name                         = "${local.project}-ca-01"
+  resource_group_name          = azurerm_resource_group.this.name
+  container_app_environment_id = azurerm_container_app_environment.this.id
+  workload_profile_name        = "Consumption"
+  tags                         = local.tags
 
-#     ingress {
-#     allow_insecure_connections = false
-#     external_enabled           = true
-#     target_port                = 443
-#     traffic_weight {
-#       percentage      = 100
-#       latest_revision = true
-#     }
-#   }
+  identity {
+    type = "SystemAssigned"
+  }
 
-#   revision_mode = "Single"
+  ingress {
+    allow_insecure_connections = false
+    external_enabled           = true
+    target_port                = local.port
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
+    }
+  }
 
-#   template {
-#     termination_grace_period_seconds = 30
-#     min_replicas = 0
-#     max_replicas = 1
+  revision_mode = "Single"
 
-#     container {
-#       name = "app"
-#       image = ""
-#       cpu = "1.0"
-#       memory = "2Gi"
-#     }
-#   }
+  template {
+    termination_grace_period_seconds = 15
+    min_replicas                     = 0
+    max_replicas                     = 1
 
-#   lifecycle {
-#     ignore_changes = [
-#       # The image is not managed by Terraform, but instead updated by CD pipelines
-#       template[0].container[0].image
-#     ]
-#   }
-# }
+    container {
+      name   = "poc-app"
+      image  = "ghcr.io/pagopa/poc-envvars:latest"
+      cpu    = "0.75"
+      memory = "1.5Gi"
+
+      startup_probe {
+        failure_count_threshold = 10
+        initial_delay           = 5
+        interval_seconds        = 3
+        path                    = "/healthz"
+        port                    = local.port
+        transport               = "HTTP"
+      }
+
+      liveness_probe {
+        failure_count_threshold = 3
+        initial_delay           = 30
+        interval_seconds        = 10
+        path                    = "/healthz"
+        port                    = local.port
+        transport               = "HTTP"
+      }
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      # The image is not managed by Terraform, but instead updated by CD pipelines
+      template[0].container[0].image
+    ]
+  }
+}
