0017eaeac9b7fe77e8db0d964dded8c07b5a7ec0
Krusty93 <andrea.grillo@pagopa.it>
2025-09-23 10:01:15 +0200

    add roles

--- infra/resources/poc_vars/iam.tf
+++ infra/resources/poc_vars/iam.tf
@@ -11,3 +11,17 @@ resource "azurerm_role_assignment" "kv_appcs" {
 #   principal_id         = azurerm_container_app.this.identity[0].principal_id
 #   description          = "Allow Container App to read App Configuration"
 # }
+
+resource "azurerm_role_assignment" "appcs_me_contributor" {
+  scope                = azurerm_app_configuration.this.id
+  role_definition_name = "App Configuration Contributor"
+  principal_id         = local.me
+  description          = "Allow me to manage App Configuration"
+}
+
+resource "azurerm_role_assignment" "appcs_me_data_owner" {
+  scope                = azurerm_app_configuration.this.id
+  role_definition_name = "App Configuration Data Owner"
+  principal_id         = local.me
+  description          = "Allow me to manage data in App Configuration"
+}
