e4daad23579901d472276129bb0d71b753e6de33
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-10-07 11:56:29 +0200

    fix code

--- apps/azure-function-v3/src/adapters/express/index.ts
+++ apps/azure-function-v3/src/adapters/express/index.ts
@@ -1,25 +1,7 @@
 import { AzureFunction, Context as FunctionContext } from "@azure/functions";
 import { withOtelContextFunctionV3 } from "@pagopa/azure-tracing/azure-functions/v3";
 import createAzureFunctionHandler from "@pagopa/express-azure-functions/dist/src/createAzureFunctionsHandler.js";
-import { Capabilities, listTasks } from "@to-do/domain";
-import express, { Express } from "express";
-import * as E from "fp-ts/lib/Either.js";
-
-export const makeExpressApp = (env: Capabilities) => {
-  const app = express();
-
-  app.use(express.json());
-  app.get("/api/tasks", async (req, res) => {
-    const tasks = await listTasks()(env)();
-    if (E.isLeft(tasks)) {
-      return res.status(500).json({ message: "Error fetching tasks" });
-    } else {
-      res.json(tasks.right);
-    }
-  });
-
-  return app;
-};
+import { Express } from "express";
 
 export const expressToAzureFunction =
   (app: Express): AzureFunction =>
