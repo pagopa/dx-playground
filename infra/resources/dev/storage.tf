b67223051451629883592ffa53e4dbf71992d453
Danilo Spinelli <gunzip@users.noreply.github.com>
2026-02-05 18:34:14 +0100

    Update workflow references and add storage module configuration (#331)
    Update workflow files to reference the 'chores/hardening' branch and
    introduce a new storage module configuration in the infrastructure
    setup.

---
+++ infra/resources/dev/storage.tf
@@ -0,0 +1,18 @@
+module "storage" {
+  source  = "pagopa-dx/azure-storage-account/azurerm"
+  version = "~> 2.0"
+
+  force_public_network_access_enabled = false
+
+  resource_group_name = local.resource_group_name
+  environment         = merge(local.environment, { app_name = "pg" })
+
+  subservices_enabled = {
+    blob  = true
+    queue = true
+    table = true
+  }
+  subnet_pep_id = data.azurerm_subnet.pep_snet.id
+  tags          = local.tags
+
+}
