98f6b831ddbe3caf85260704258bb130138f6826
Marco Comi <marco.comi@pagopa.it>
2026-02-17 13:06:51 +0100

    Remove random provider

--- infra/resources/dev/secrets.tf
+++ infra/resources/dev/secrets.tf
@@ -1,12 +1,7 @@
-ephemeral "random_password" "ephemeral_subscription_key" {
-  length           = 16
-  override_special = "!#$%&*()-_=+[]{}<>:?"
-}
-
 resource "azurerm_key_vault_secret" "todo_webapp_apim_key" {
   key_vault_id     = data.azurerm_key_vault.common_kv.id
   name             = "todo-webapp-apim-key"
-  value_wo         = ephemeral.random_password.ephemeral_subscription_key.result
+  value_wo         = ""
   value_wo_version = 1
   content_type     = "The subscription key for the ToDo Web App to access the ToDo API (through APIM)"
 }
