b3b3d8eef798dbc7337f283e008c2493c3d2fefd
Christian Calabrese <christian.calabrese@pagopa.it>
2026-03-16 11:24:52 +0100

    Point to feature branch

--- infra/resources/dev/postgres.tf
+++ infra/resources/dev/postgres.tf
@@ -9,7 +9,7 @@ ephemeral "random_password" "admin_password" {
 }
 
 module "postgres" {
-  source = "github.com/pagopa/dx//infra/modules//azure_postgres_server?ref=793326b44f041572e1cc44dafa4ac982a5f2d389"
+  source = "github.com/pagopa/dx//infra/modules//azure_postgres_server?ref=refactors%2Fpostgres-module"
 
   tags = local.tags
   environment = {
