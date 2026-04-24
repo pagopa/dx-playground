9a5c2b2899fc31a1d3879f41c7f105073b0da415
Marco Comi <marco.comi@pagopa.it>
2026-04-24 12:50:42 +0200

    Dynamic load instrumentation

--- apps/to-do-webapp/src/instrumentation.ts
+++ apps/to-do-webapp/src/instrumentation.ts
@@ -1,3 +1,11 @@
 export async function register() {
-  // initAzureMonitor();
+  const [{ initAzureMonitor }] = await Promise.all([
+    import("@pagopa/azure-tracing/azure-monitor"),
+  ]);
+
+  initAzureMonitor([]);
+
+  console.log(
+    "Azure Monitor OpenTelemetry has been initialized on the server.",
+  );
 }
